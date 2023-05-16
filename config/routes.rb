dda617cc3c8d5b1981d97629483efb723d82d0d9
thadeus <thadeusnyariki94@gmail.com>
2023-05-17 00:53:11 +0300

    get started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  #root route
+  root 'demo#index'
+  get 'demo/index'
+  # get 'demo/index' #=> simple route
+
+  # get ':controller(/:action(/:id))' #=> Default route - may go away in future rails
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
